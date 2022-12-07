316d0e8bdbfd7154c56769d4b493f5ce63acb1bf
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-07 18:23:42 -0300

    :sparkles: feat: post category controller

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -1,12 +1,24 @@
-import * as allUrls from '../resositories/categories.repository.js'
+import * as allCategories from "../resositories/categories.repository.js";
 
 async function getCategoryController(req, res) {
   try {
-    const result = await allUrls.getCategoryRepoditory();
+    const result = await allCategories.getCategoryRepoditory();
+    
     return res.send(result.rows);
   } catch (error) {
     return res.sendStatus(500).send(error);
   }
 }
+async function postCategoryController(req, res) {
+  let { name } = req.body
+  name = name.trim().replace(/( )+/g, " ");
+  try {
+    await allCategories.postCategoryRepoditory(name);
+   
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.sendStatus(500).send(error);
+  }
+}
 
-export { getCategoryController };
+export { getCategoryController, postCategoryController };
