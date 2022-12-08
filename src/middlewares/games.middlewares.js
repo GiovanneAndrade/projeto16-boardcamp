d6c623054b8728d2f49434d08b54ae80d02c63f6
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-08 20:02:31 -0300

    :sparkles: feat: post games middlewares

---
+++ src/middlewares/games.middlewares.js
@@ -0,0 +1,30 @@
+import * as allCategories from "../resositories/categories.repository.js";
+import { postGamesSchema } from "../schemas/games.schema.js";
+
+const postGamesMiddlewares = async function (req, res, next) {
+  let { name, categoryId } = req.body;
+  let consult = "games";
+  const resultGames = await allCategories.getCategoryRepository(consult);
+  name = name.replace(/[^a-z0-9]/gi, "");
+  const validationName = resultGames.rows.filter(
+    (i) => i.name.replace(/[^a-z0-9]/gi, "") === name
+  );
+  if (validationName.length > 0) {
+    return res.sendStatus(409);
+  }
+  
+  consult = "categories";
+  const resultCategories = await allCategories.getCategoryRepository(consult);
+  const validationCategories = resultCategories.rows.filter(
+    (i) => i.id === categoryId
+  );
+
+  const valiCadastro = postGamesSchema.validate(req.body, {
+    abortEarly: false,
+  });
+  if (valiCadastro.error || validationCategories.length === 0) {
+    return res.sendStatus(400);
+  }
+  next();
+};
+export { postGamesMiddlewares };
