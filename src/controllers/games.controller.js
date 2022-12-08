0990ec47d0522ddfa5533528739d46afcf0cb3f6
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-08 20:02:46 -0300

    :sparkles: feat: post games controllers

---
+++ src/controllers/games.controller.js
@@ -0,0 +1,35 @@
+import * as allGames from "../resositories/games.repository.js";
+
+async function getGamesController(req, res) {
+  const { name } = req.query
+  if(name){
+    const resultGameQuery = await allGames.getQueryGamesRepoditory(name);
+    return res.send(resultGameQuery.rows);
+  }
+  try {
+    const result = await allGames.getGamesRepoditory();
+    return res.send(result.rows);
+  } catch (error) {
+    return res.sendStatus(500).send(error);
+  }
+}
+
+async function postGamesController(req, res) {
+  let { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+  name = name.trim().replace(/( )+/g, " ");
+  try {
+    await allGames.postGamesRepository({
+      name,
+      image,
+      stockTotal,
+      categoryId,
+      pricePerDay,
+    });
+
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.sendStatus(500).send(error);
+  }
+}
+
+export { getGamesController, postGamesController };
