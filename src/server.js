75f0449ce6bf9ada31be63d18ac8aaa328302be2
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-09 20:04:36 -0300

    :sparkles: feat: customers router server

--- src/server.js
+++ src/server.js
@@ -2,13 +2,14 @@ import  express  from "express";
 import cors from "cors";
 import categoriesRouter from './routers/categories.router.js'
 import gamesRouter from './routers/games.router.js'
-
+import customersRouter from './routers/customers.router.js'
 const app = express();
 app.use(cors());
 app.use(express.json());
-  
+   
 app.use(categoriesRouter)
 app.use(gamesRouter)
+app.use(customersRouter)
 
 app.listen(4000, ()=>{ 
   console.log("listening on port 4000" );
