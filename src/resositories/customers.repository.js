6fd96c66d814d0e34b72bf7fec77030bbaf67291
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-09 20:00:23 -0300

    :sparkles: feat: post custmers repository

--- src/resositories/customers.repository.js
+++ src/resositories/customers.repository.js
@@ -10,6 +10,16 @@ async function getCustomersRepository() {
   return result;
 }
 
+async function postCustomersRepository({ name, phone, cpf, birthday }) {
+  const result = await connection.query(
+    `
+      INSERT INTO customers ( name, phone, cpf, birthday )
+      VALUES ($1, $2, $3, $4)
+   `,
+    [name, phone, cpf, birthday]
+  );
 
+  return result;
+}
 
-export { getCustomersRepository };
+export { getCustomersRepository, postCustomersRepository };
