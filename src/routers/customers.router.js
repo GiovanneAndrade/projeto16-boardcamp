a58a3eaf3c0e563eb615e8c83a3d9da1157081be
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-10 12:14:46 -0300

    :sparkles: feat: get id customers router

--- src/routers/customers.router.js
+++ src/routers/customers.router.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
-import { getCustomersController, postCustomersController } from "../controllers/customers.controller.js";
-import { postCustomersMiddlewares } from "../middlewares/customers.middlewares.js";
+import { getCustomersController, getIdCustomersController, postCustomersController, putCustomersController } from "../controllers/customers.controller.js";
+import { getIdCustomersMiddlewares, postCustomersMiddlewares } from "../middlewares/customers.middlewares.js";
  
 
 const router = Router();
 
 router.get("/customers", getCustomersController );
+router.get("/customers/:id",getIdCustomersMiddlewares, getIdCustomersController );
 router.post("/customers",postCustomersMiddlewares, postCustomersController );
+
  
 
 export default router;
