2c9b9238208b0dfae7586f816f8a554f12b2230a
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-11 15:59:40 -0300

    :sparkles: feat: post  rentals router

---
+++ src/routers/rentals.router.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { getRentalsController, postRentalsController } from "../controllers/rentals.controller.js";
+import { postRentalsMiddlewares } from "../middlewares/rentals.middlewares.js";
+ 
+ 
+
+const router = Router();
+
+router.get("/rentals", getRentalsController);
+router.post("/rentals",postRentalsMiddlewares, postRentalsController);
+ 
+
+export default router;
