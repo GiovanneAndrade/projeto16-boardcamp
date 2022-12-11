a4e3fdbb060808dbbf237b593fd184c4820bba66
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-11 16:05:22 -0300

    :sparkles: feat:   rentals router server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import categoriesRouter from './routers/categories.router.js'
 import gamesRouter from './routers/games.router.js'
 import customersRouter from './routers/customers.router.js'
+import rentalsRouter from './routers/rentals.router.js'
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,6 +13,7 @@ app.use(express.json());
 app.use(categoriesRouter)
 app.use(gamesRouter)
 app.use(customersRouter)
+app.use(rentalsRouter)
 
 app.listen(4000, ()=>{ 
   console.log("listening on port 4000" );
