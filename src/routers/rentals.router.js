151f03ea543eb1a6f3eb2567528ab22310b9d840
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-11 19:49:40 -0300

    :sparkles: feat: post  id rentals router

--- src/routers/rentals.router.js
+++ src/routers/rentals.router.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import {
  
   getRentalsController,
- 
+  postIdRentalsController,
   postRentalsController,
 } from "../controllers/rentals.controller.js";
 import { 
- 
+  
+  postIdRentalsMiddlewares, 
   postRentalsMiddlewares
 } from "../middlewares/rentals.middlewares.js";
 
@@ -14,6 +15,7 @@ const router = Router();
 
 router.get("/rentals", getRentalsController);
 router.post("/rentals", postRentalsMiddlewares, postRentalsController);
-
+router.post("/rentals/:id/return",postIdRentalsMiddlewares, postIdRentalsController);
+ 
 
 export default router;
