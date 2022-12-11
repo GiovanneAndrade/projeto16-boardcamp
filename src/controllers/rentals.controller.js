db783edd8566acec8ef5016fc5f21fe12800f0bb
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-11 19:55:08 -0300

    :sparkles: feat: post  rentals controller

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -51,8 +51,48 @@ async function getRentalsController(req, res) {
   }
 }
 
+async function postRentalsController(req, res) {
+  const { customerId, gameId, daysRented } = req.body;
+  const id = gameId;
+  const consult = "games";
+  const rentDate = new Date().toLocaleDateString();
+  const pricePerDay = await allCustomers.getIdCustomersRepository({
+    id,
+    consult,
+  });
+  const originalPrice = pricePerDay.rows[0].pricePerDay * daysRented;
+  const returnDate = null;
+  const delayFee = null;
+  try {
+    await allRentals.postRentalsRepoditory({
+      customerId,
+      gameId,
+      daysRented,
+      rentDate,
+      originalPrice,
+      returnDate,
+      delayFee,
+    });
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.sendStatus(500).send(error);
+  }
+}
+
+ 
+async function deleteIdRentalsController(req, res) {
+  const { id } = req.params;
+  try {
+    await allRentals.deleteIdRentalsRepoditory({ id });
+    return res.sendStatus(200);
+  } catch (error) {
+    return res.sendStatus(500).send(error);
+  }
+}
 
 export {
   getRentalsController,
+  postRentalsController,
  
+  deleteIdRentalsController,
 };
