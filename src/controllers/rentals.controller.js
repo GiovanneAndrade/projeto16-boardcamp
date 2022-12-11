cac94af2d009a64b9ce78c96e228a9a2d5f5ffe3
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-11 19:54:42 -0300

    :sparkles: feat: get  rentals controller

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -1,33 +1,58 @@
 import * as allRentals from "../resositories/rentals.repository.js";
 import * as allCustomers from "../resositories/customers.repository.js";
-
-
-async function postRentalsController(req, res) {
-  const { customerId, gameId, daysRented } = req.body;
-  const id = gameId;
-  const consult = "games";
-  const rentDate = new Date().toLocaleDateString();
-  const pricePerDay = await allCustomers.getIdCustomersRepository({
-    id,
-    consult,
-  });
-  const originalPrice = pricePerDay.rows[0].pricePerDay * daysRented;
-  const returnDate = null;
-  const delayFee = null;
-  try {
-    await allRentals.postRentalsRepoditory({
-      customerId,
-      gameId,
-      daysRented,
-      rentDate,
-      originalPrice,
-      returnDate,
-      delayFee,
+import dayjs from "dayjs";
+async function getRentalsController(req, res) {
+  let consult = req.query.customerId;
+  let consultWhere = "customers.id";
+  function organizedResult(result) {
+    const response = result.rows.map((rentals) => {
+      return {
+        id: rentals.id,
+        customerId: rentals.customerId,
+        gameId: rentals.gameId,
+        rentDate: rentals.rentDate,
+        daysRented: rentals.daysRented,
+        returnDate: rentals.returnDate,
+        originalPrice: rentals.originalPrice,
+        delayFee: rentals.delayFee,
+        customer: {
+          id: rentals.customerId,
+          name: rentals.customerName,
+        },
+        game: {
+          id: rentals.gameId,
+          name: rentals.gameName,
+          categoryId: rentals.categoryId,
+          categoryName: rentals.categoryName,
+        },
+      };
     });
-    return res.sendStatus(201);
+    return res.status(200).send(response);
+  }
+
+  try {
+    if (consult) {
+      const result = await allRentals.getRentalsRepoditory({
+        consult,
+        consultWhere,
+      });
+      return organizedResult(result);
+    } else {
+      consult = req.query.gameId;
+      consultWhere = "games.id";
+      const result = await allRentals.getRentalsRepoditory({
+        consult,
+        consultWhere,
+      });
+      return organizedResult(result);
+    }
   } catch (error) {
     return res.sendStatus(500).send(error);
   }
 }
 
-export {   postRentalsController };
+
+export {
+  getRentalsController,
+ 
+};
