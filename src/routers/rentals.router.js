dbe511ed5300b3b7b8dccc356a3602e04ccfc8f0
GiovanneAndrade <vanne.as@hotmail.com>
2022-12-11 19:50:10 -0300

    :sparkles: feat: delete  id rentals router

--- src/routers/rentals.router.js
+++ src/routers/rentals.router.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import {
- 
+  deleteIdRentalsController,
   getRentalsController,
   postIdRentalsController,
   postRentalsController,
 } from "../controllers/rentals.controller.js";
 import { 
-  
+  deleteIdRentalsMiddlewares,
   postIdRentalsMiddlewares, 
   postRentalsMiddlewares
 } from "../middlewares/rentals.middlewares.js";
@@ -16,6 +16,6 @@ const router = Router();
 router.get("/rentals", getRentalsController);
 router.post("/rentals", postRentalsMiddlewares, postRentalsController);
 router.post("/rentals/:id/return",postIdRentalsMiddlewares, postIdRentalsController);
- 
+router.delete("/rentals/:id", deleteIdRentalsMiddlewares,  deleteIdRentalsController);
 
 export default router;
